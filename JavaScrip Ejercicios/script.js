042210ddbd8f7614958d22a460605289f6c0eb17
Irene <ireneneri001@gmail.com>
2022-09-30 11:46:11 +0200

    Add files via upload

---
+++ JavaScrip Ejercicios/script.js
@@ -0,0 +1,16 @@
+
+var Lista1 = new Array [56, 0, 4, 5, 77, 345, 23, 23, 33, 56, 0, 13, 14, 15, 5, 4, 8];
+var Lista2 = new Array [0, 0, 4, 4, 5, 5, 8, 13, 14, 15, 23, 23, 33, 56, 56, 77, 345];
+var Lista3 = new Array [345, 77, 56, 56, 33, 23, 23, 15, 14, 13, 8, 5, 5, 4, 4, 0, 0];
+//var Lista4 = 0(2) 4(2) 5(2) 8(1) 13(1) 14(1) 15(1) 23(2) 33(1) 56(2) 77(1) 345(1);
+
+
+//Lista2.sort();
+//Lista3.sort();
+//Lista3.reverse();
+
+function ordenaValores(int[]array){
+    array.sort();
+    array.reverse();
+}
+
